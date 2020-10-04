e2a38de0b4afca4a182bf7fc824f4880bfb43fe4
lukemartinlogan <lukemartinlogan@gmail.com>
2020-10-04 11:52:03 -0500

    Actually add cmake

---
+++ build/CMakeFiles/libnvmed.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libnvmed.dir/library/lib_nvmed.c.o"
+  "CMakeFiles/libnvmed.dir/library/utils/radix-tree.c.o"
+  "liblibnvmed.a"
+  "liblibnvmed.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/libnvmed.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
